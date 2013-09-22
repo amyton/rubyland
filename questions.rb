a1907c391dc8ea1b1cb24cb03172d5a11dc692e1
Amy Ton <amyhton@gmail.com>
2013-09-22 11:24:14 -0500

    Add questions hash

--- questions.rb
+++ questions.rb
@@ -1,6 +1,7 @@
 class Question 
   attr_accessor :text, :answers, :difficulty
   attr_accessor :easy, :medium, :hard
+  attr_accessor :choices
 
   def initialize(text, difficulty)
     @text = text
@@ -13,6 +14,12 @@ class Question
     @choices.push(text, correct)
   end
 
+  def show_choices
+    @choices.text do |choice|
+        puts choice.text
+    end
+  end
+
   # select the correct answer in the choices array.
   # the first add_choices is the correct one
   private
@@ -21,9 +28,13 @@ class Question
   end
 end
 
-# Check to see if user "answer" is the correct answer
-def answer_correct?(answer)
-  answer == correct_answer
+# Check to see if (user) "answer" is the correct answer
+def answer_correct?(user_answer)
+  if user_answer == correct_answer
+    puts "correct!"
+  else
+    puts "incorrect!"
+  end
 end
 
 q1 = Question.new("Naughty method! Do you know which method has exhibitionist-like qualities?", :easy)
@@ -33,13 +44,26 @@ q1.add_choices("public", false)
 q1.add_choices("gets.chomp", false)
 
 
-# puts q1.text
-# answer = gets.chomp
+puts q1.text
+puts q1.choices
 
-# if q1.answer_correct?(answer)
-#   puts "correct!"
-# else
-#   puts "incorrect!"
-# end
+user_answer = gets.chomp
+
+
+
+------
+questions = {
+  easy: {
+    question: "What is ",
+    answers: [
+      ["red", true],
+      ["blue", false],
+      ["yellow", false],
+      ["green", false],
+
+    ]
+  }
+}
+
 
 
