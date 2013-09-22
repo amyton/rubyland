e40ca3b24a44476f0614885d0e92c149484dc5c9
beccasheldon <rebecca.e.sheldon@gmail.com>
2013-09-22 11:47:59 -0500

    creating form and logic for question thang

--- adventuresinrubyland.rb
+++ adventuresinrubyland.rb
@@ -3,6 +3,8 @@ require 'bundler/setup'
 require 'sinatra'
 require 'sinatra/reloader'
 
+"checking ghost"
+
 get '/' do
 
 end
@@ -21,7 +23,47 @@ end
 #   @hashtag2 = [:hashtag2]
 #   erb :twitter
 
-  # class tweets 
-  #   []
-  # end
+class Question 
+  attr_accessor :text, :answers, :difficulty
+  attr_accessor :easy, :medium, :hard
+
+  def initialize(text, difficulty)
+    @text = text
+    @answers = []
+    @difficulty = :easy, :medium, :hard
+  end
+
+  # add answers to answers array
+  def add_answer(text, correct)
+    @answers.push(text, correct)
+  end
+
+  # Check to see if "answer" 
+  # is the correct answer
+  def answer_correct?(answer)
+    answer == correct_answer
+  end
+
+  private
+  def correct_answer
+    @answers.select { |a| a.correct }.first.text
+  end
+end
+
+q1 = Question.new("Naughty method! Do you know which method has exhibitionist-like qualities?", :easy)
+q1.add_answer("attr_accessor", true)
+q1.add_answer("xxx.flash", false)
+q1.add_answer("public", false)
+q1.add_answer("gets.chomp", false)
+
+
+# puts q1.text
+# answer = gets.chomp
+
+# if q1.answer_correct?(answer)
+#   puts "correct!"
+# else
+#   puts "incorrect!"
 # end
+
+
