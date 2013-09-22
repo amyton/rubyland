18a0a84917d432826c875205ba0803d7de0d99a7
Amy Ton <amyhton@gmail.com>
2013-09-22 10:24:24 -0500

    Rename add_answers to add_choices

--- questions.rb
+++ questions.rb
@@ -6,39 +6,43 @@ class Question
     @text = text
     @answers = []
     @difficulty = :easy, :medium, :hard
+
+    puts text
   end
 
   # add answers to answers array
-  def add_answer(text, correct)
+  def add_choices(text, correct)
     @answers.push(text, correct)
   end
 
-  # Check to see if "answer" 
-  # is the correct answer
+  # Check to see if "answer" is the correct answer
   def answer_correct?(answer)
     answer == correct_answer
   end
 
+  #
+  #
   private
   def correct_answer
     @answers.select { |a| a.correct }.first.text
   end
+  
 end
 
 q1 = Question.new("Naughty method! Do you know which method has exhibitionist-like qualities?", :easy)
-q1.add_answer("attr_accessor", true)
-q1.add_answer("xxx.flash", false)
-q1.add_answer("public", false)
-q1.add_answer("gets.chomp", false)
+q1.add_choices("attr_accessor", true)
+q1.add_choices("xxx.flash", false)
+q1.add_choices("public", false)
+q1.add_choices("gets.chomp", false)
 
 
-# puts q1.text
-# answer = gets.chomp
+puts q1.text
+answer = gets.chomp
 
-# if q1.answer_correct?(answer)
-#   puts "correct!"
-# else
-#   puts "incorrect!"
-# end
+if q1.answer_correct?(answer)
+  puts "correct!"
+else
+  puts "incorrect!"
+end
 
 
