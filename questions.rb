8527034b7df318cde87ebd43aca39219dc065707
Amy Ton <amyhton@gmail.com>
2013-09-21 20:19:35 -0500

    Update the changes to match game.rb

--- questions.rb
+++ questions.rb
@@ -1,67 +1,44 @@
-  game.rb > game_spec = write test
-  question class
-    create new question
-      question - MC
-        how would this work?
-        diffcult level woult be attribute of each question
-        each question would have easy attribute, med attribute, hard attribute
+class Question 
+  attr_accessor :text, :answers, :difficulty
+  attr_accessor :easy, :medium, :hard
 
-        question class - have  question would have answer attribute, which would be array of other answer objects
-
-
-        answer class - correct or incorrect
-
-
-
-
-class Question
-  @test 
-  @answers = []
-  @difficulty
-    :easy, :medium, :hard
-
-# creating a question
-  Question.new("What's ruby?", :easy)
-  q.add_answer("4", true) // because answer is an array - add books like on library
-  q.add_answer("6", false)
-
-
-  puts q.text (output the question, whats ruby?)
-  answer = gets.chomp
-
-  if q.answer_correct?(answer)
-    puts "correct"
-  else
-    puts "Incorrect"
+  def initialize(text, difficulty)
+    @text = text
+    @answers = []
+    @difficulty = :easy, :medium, :hard
   end
 
-  def add_answer(text, correct)  ++ test  - make sure when incorrect text, the answer array... when test answer correct, set up questions and answers. call correct answer with question..5
-    @answers << 
-      Answer.new(text, correst)
-    end
+  # add answers to answers array
+  def add_answer(text, correct)
+    @answers.push(text, correct)
   end
 
-  def answer_correct?(answer) - check to see if this answer is the 
-    same as the correct answer ++ test
-    answer == correct_answer // then it will return true
+  # Check to see if "answer" 
+  # is the correct answer
+  def answer_correct?(answer)
+    answer == correct_answer
   end
 
-  private ++ dont need to test
+  private
   def correct_answer
-    @answers.select do |something|
-      a.correct.first.text
-    end // select always return out the array
+    @answers.select { |a| a.correct }.first.text
   end
+end
+
+q1 = Question.new("Naughty method! Do you know which method has exhibitionist-like qualities?", :easy)
+q1.add_answer("attr_accessor", true)
+q1.add_answer("xxx.flash", false)
+q1.add_answer("public", false)
+q1.add_answer("gets.chomp", false)
 
 
+# puts q1.text
+# answer = gets.chomp
 
-
-class Answer
-  @text
-  @correct
-    true or flase
-
-
-
+# if q1.answer_correct?(answer)
+#   puts "correct!"
+# else
+#   puts "incorrect!"
+# end
 
 
