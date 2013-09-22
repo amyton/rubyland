2fa3a33875864c6eed5bcdc099c060a8001bb4a0
Amy Ton <amyhton@gmail.com>
2013-09-22 10:31:29 -0500

    Rename some variables for better clarification.

--- questions.rb
+++ questions.rb
@@ -4,32 +4,33 @@ class Question
 
   def initialize(text, difficulty)
     @text = text
-    @answers = []
+    @choices = []
     @difficulty = :easy, :medium, :hard
   end
 
-  # add answers to answers array
-  def add_answer(text, correct)
-    @answers.push(text, correct)
+  # add choices to answers array
+  def add_choices(text, correct)
+    @choices.push(text, correct)
   end
 
-  # Check to see if "answer" 
-  # is the correct answer
-  def answer_correct?(answer)
-    answer == correct_answer
-  end
-
+  # select the correct answer in the choices array.
+  # the first add_choices is the correct one
   private
   def correct_answer
-    @answers.select { |a| a.correct }.first.text
+    @choices.select { |answer| answer.correct }.first.text
   end
 end
 
+# Check to see if user "answer" is the correct answer
+def answer_correct?(answer)
+  answer == correct_answer
+end
+
 q1 = Question.new("Naughty method! Do you know which method has exhibitionist-like qualities?", :easy)
-q1.add_answer("attr_accessor", true)
-q1.add_answer("xxx.flash", false)
-q1.add_answer("public", false)
-q1.add_answer("gets.chomp", false)
+q1.add_choices("attr_accessor", true)
+q1.add_choices("xxx.flash", false)
+q1.add_choices("public", false)
+q1.add_choices("gets.chomp", false)
 
 
 # puts q1.text
